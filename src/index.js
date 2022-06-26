edd23a89580b61a2ffb5c54b763040a451f793c3
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-27 02:48:00 +0300

    Added an event listener for the form submit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ let f = document.querySelector("form")
 let c = document.querySelector("#tasks");
 let val = document.querySelector("#new-task-description");
 
-
+//Added an event listener for the form submit
+f.addEventListener("submit", e=>{
+  e.preventDefault();
+  //checked if a description was entered and handled it
+  if(val.value.length != 0) create(val.value);
+  else {alert("Please enter a description"); return}
+  f.reset();
+})
 
 
 

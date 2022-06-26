bba54ad9d6edfffc298864e867bc9395c339429d
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-27 02:47:16 +0300

    Got required forms, inputs and containers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+//Got required forms, inputs and containers
+let f = document.querySelector("form")
+let c = document.querySelector("#tasks");
+let val = document.querySelector("#new-task-description");
+
+
+
+
+
 });

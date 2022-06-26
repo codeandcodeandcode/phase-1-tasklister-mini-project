543d5bebc0bc78c684f003eb0605ac94308a8e15
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-27 02:51:12 +0300

    Function to handle task click & Added a function to create the task and append it's content to the dom

--- src/index.js
+++ src/index.js
@@ -19,8 +19,22 @@ const deleteEl = (el)=>{
   el.remove();
 }
 
+//Function to handle task click
+const  handleClick = (t, el)=>{
+  if(t.target.classList.contains("delete")) deleteEl(el)
+}
 
-
-
+//Added a function to create the task and append it's content to the dom
+const create = (data)=> {
+  let a = document.createElement("div");
+  a.innerHTML = `<p>${data}</p><div><button class="delete">Delete</button></div>`;
+  a.style.display="flex";
+  a.style.justifyContent="space-between";
+  a.style.alignItems = "center";
+  a.addEventListener("click", e=>{
+    handleClick(e, a);
+    })
+    c.prepend(a)
+  }
 
 });
